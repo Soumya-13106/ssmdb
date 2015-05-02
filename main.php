8d76d9802132dd3a5aa185dd2f52bf886b0e6bf1
Soumya-13106 <soumya13106@iiitd.ac.in>
2015-05-02 22:03:47 +0530

    Left side resolved. Right side add option made

--- main.php
+++ main.php
@@ -3,13 +3,13 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript">
 		$('#selectDBTag').change(function() {
-   		var parent = $(this).val();
-   		
-   		$('#payment_plan').children().each(function() {
-      		if($(this).data('parent') != parent) {
-                $(this).hide();
-   			} else    $(this).show();
-   		});
+		var parent = $(this).val();
+		
+		$('#payment_plan').children().each(function() {
+			if($(this).data('parent') != parent) {
+				$(this).hide();
+			} else    $(this).show();
+		});
 	});
 	</script>-->
 </head>
@@ -33,74 +33,98 @@
 
 	$result = $mysqli->query("USE dbms_project1");
 	if (!$result) {
-	    echo 'Could not run query: ' . mysql_error();
-	    exit;
+		echo 'Could not run query: ' . mysql_error();
+		exit;
 	}
 
 ?>
 	<div id="modal">
 		<div id="left">
 		<form action='#' method="POST">
-		  <div class='field'>
-		    <input placeholder="Query" name="query">
-		    <label>Your Query</label>
-		  </div>
-		  <div class='field form-actions'>
-		    <button type='submit'>Execute</button>
-		  </div>
+			<div class='field'>
+			<input placeholder="Query" name="query">
+			<label>Your Query</label>
+			</div>
+			<div class='field form-actions'>
+			<button type='submit' name='customQuery'>Execute</button>
+			</div>
 
-		  <?php
-		  	$result = $mysqli->query($_POST["query"]);
-	if (!$result) {
-	    echo 'Could not run query: ' . mysql_error();
-	    exit;
-	}
+			<?php
+			if(isset($_POST["customQuery"])){
+				$result = $mysqli->query($_POST["query"]);
+				if (!$result) {
+					echo 'Could not run query: ' . mysql_error();
+					exit;
+				}
 
-	echo "<table>";
-	$finfo = $result->fetch_fields();
-	echo "<tr>";
-	foreach ($finfo as $val) {
-		echo "<td>" . $val->name . "</td>";
-	}
-	echo "</tr>";
-
-	while($row = $result->fetch_array()) {
-    	$finfo = $result->fetch_fields();
-		echo "<tr>";
-		foreach ($finfo as $val) {
-			echo "<td>" . $row[$val->name] . "</td>";
-		}
-		echo "</tr>";
-	}
-	echo "</table>";
-
-		  ?>
+				echo "<table>";
+				$finfo = $result->fetch_fields();
+				echo "<tr>";
+				foreach ($finfo as $val) {
+					echo "<td>" . $val->name . "</td>";
+				}
+				echo "</tr>";
+				while($row = $result->fetch_array()) {
+					$finfo = $result->fetch_fields();
+					echo "<tr>";
+					foreach ($finfo as $val) {
+						echo "<td>" . $row[$val->name] . "</td>";
+					}
+					echo "</tr>";
+				}
+				echo "</table>";
+			}
+			?>
 		</form>
 		</div>
 		<div id="center">
 
 		</div>
 		<div id="right">
-			<button type="button" id="addEntry" onclick="document.getElementById('selectDB').style.display='block';document.getElementById('executeQuery').style.display='block';">Add</button>
-			<button type="button" id="updateEntry" onclick="document.getElementById('selectDB').style.display='block';document.getElementById('executeQuery').style.display='block';">Update</button>
-			<button type="button" id="deleteEntry" onclick="document.getElementById('selectDB').style.display='block';document.getElementById('executeQuery').style.display='block';">Delete</button>
 			<div id="selectDB">
+			<form method="POST" action="#">
 				<?php
 				$res = $mysqli->query("SHOW TABLES");
-  				echo "<select name='selectDBTag' id='selectDBTag'>";
-  				while($cRow = mysqli_fetch_array($res)) {
-    				echo "<option value=" . $cRow[0] . ">" . $cRow[0] ."</option>";
+				echo "<select name='selectDBTag' id='selectDBTag'>";
+				while($cRow = mysqli_fetch_array($res)) {
+					echo "<option value=" . $cRow[0] . ">" . $cRow[0] ."</option>";
 				}
 				echo "</select>"
-
-
-
 				?>
-
+			<button type="submit" name="addEntry" id="addEntryID" onclick="document.getElementById('selectDB').style.display='block';document.getElementById('executeQuery').style.display='block';">Add</button>
+			<button type="submit" name="updateEntry" id="updateEntryID" onclick="document.getElementById('selectDB').style.display='block';document.getElementById('executeQuery').style.display='block';">Update</button>
+			<button type="submit" name="deleteEntry" id="deleteEntryID" onclick="document.getElementById('selectDB').style.display='block';document.getElementById('executeQuery').style.display='block';">Delete</button>
+			</form>
 			</div>
-			<button type="button" id="executeQuery" onclick="document.getElementById('selectDB').style.display='block';">Execute</button>
-		</div>
-	</div>
+			<form name='selectQueryForm' action='#' method='POST'>				
+			<?php
+			//echo $_POST['addEntry'];
+			if(isset($_POST['addEntry'])) {
+				$result = $mysqli->query("SELECT * from " . $_POST['selectDBTag']);
+				if (!$result) {
+					echo 'Could not run query: ' . mysql_error();
+					exit;
+				}
+
+				$finfo = $result->fetch_fields();
+				foreach ($finfo as $val) {
+					echo "<input class='attributeTextField' type='text' placeholder=" . $val->name . " name=" . $val->name . "</input>";
+				}
+				echo "<table>";
+				while($row = $result->fetch_array()) {
+					$finfo = $result->fetch_fields();
+					echo "<tr>";
+					foreach ($finfo as $val) {
+						echo "<td>" . $row[$val->name] . "</td>";
+					}
+					echo "</tr>";
+				}
+				echo "</table>";
+
+			}
+			?>
+			</form>
+			</div>
 	
 
 
@@ -109,8 +133,8 @@
 	//echo "Yay";
 	$result = $mysqli->query("SELECT * from actor");
 	if (!$result) {
-	    echo 'Could not run query: ' . mysql_error();
-	    exit;
+		echo 'Could not run query: ' . mysql_error();
+		exit;
 	}
 
 	echo "<table>";
@@ -122,7 +146,7 @@
 	echo "</tr>";
 
 	while($row = $result->fetch_array()) {
-    	$finfo = $result->fetch_fields();
+		$finfo = $result->fetch_fields();
 		echo "<tr>";
 		foreach ($finfo as $val) {
 			echo "<td>" . $row[$val->name] . "</td>";
